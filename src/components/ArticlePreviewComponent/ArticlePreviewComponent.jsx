d7bd9514a61f5115fff3d65224d8e303699f67a5
Mihai Hiticas <hiticasmihai7@gmail.com>
2025-12-31 20:55:22 +0200

    finish mobile version

--- src/components/ArticlePreviewComponent/ArticlePreviewComponent.jsx
+++ src/components/ArticlePreviewComponent/ArticlePreviewComponent.jsx
@@ -2,17 +2,20 @@ import "./ArticlePreviewComponent.scss"
 import previewImage from "../../assets/images/drawers.jpg";
 import avatarMichelle from "../../assets/images/avatar-michelle.jpg";
 import iconShare from "../../assets/images/icon-share.svg";
+import iconFacebook from "../../assets/images/icon-facebook.svg";
+import iconPinterest from "../../assets/images/icon-pinterest.svg";
+import iconTwitter from "../../assets/images/icon-twitter.svg";
 import { useState } from 'react';
 
 function ArticlePreviewComponent() {
   
   //const [showTooltip, setShowTooltip] = useState(false);
-  //const [showOverlay, setShowOverlay] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(true);
 
   const handleShareClick = () => {
     console.log("Share button clicked");
     // setShowTooltip(!showTooltip);
-    //setShowOverlay(!showOverlay);
+    setShowOverlay(!showOverlay);
   };
 
   return (
@@ -28,12 +31,24 @@ function ArticlePreviewComponent() {
             <p className="publish-date text-preset-2">28 Jun 2020</p>
           </div>
           <div className="share-button-wrapper">
-            <div className="share-button" aria-label="Share Article" onClick={handleShareClick}>
+            <button className="share-button" aria-label="Share Article" onClick={handleShareClick}>
               <img src={iconShare} alt="Share Icon" />
               {/*<div className={`share-tooltip text-preset-3 ${showTooltip ? 'show' : ''}`}>Share tablet/desktop</div>*/}
+            </button>
+          </div>
+          <div className={`share-overlay text-preset-3 ${showOverlay ? 'show' : ''}`}>
+            <p className="share-text text-preset-2">Share</p>
+            <div className="share-icons">
+              <img src={iconFacebook} alt="Share Icon" />
+              <img src={iconPinterest} alt="Share Icon" />
+              <img src={iconTwitter} alt="Share Icon" />
+            </div>
+            <div className="share-button-wrapper">
+              <button className="share-button" aria-label="Share Article" onClick={handleShareClick}>
+                <img src={iconShare} alt="Share Icon" />
+              </button>
             </div>
           </div>
-          {/*<div className={`share-overlay text-preset-3 ${showOverlay ? 'show' : ''}`}>Share mobile</div>*/}
         </div>
       </div>
     </div>
